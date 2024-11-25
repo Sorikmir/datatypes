2e87273b22f7b4a8a10bf30da096a077216494e5
Sorikmir <sergiimyr@gmail.com>
2024-11-24 20:27:33 -0500

    tasks from learn js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,67 @@
-console.log("hello World");
-let num;
-num = 300 + 500;
-alert (num);
+// function askJavaScriptName() {
+
+//     let userAnswer = prompt("Какое «официальное» название JavaScript?");
+//     if (userAnswer === "ECMAScript") {
+//         alert("Верно!");
+//         return; 
+//     } else {
+//         alert(" Не знаете? ECMAScript!");
+//     }
+//      askJavaScriptName();
+// }
+
+
+//     askJavaScriptName();
+
+
+//     function checkNumber() {
+    
+    
+    
+//     let number = prompt("Введите число:");
+//     if (number > 0) {
+//         alert(1);
+//     } 
+//     else if (number < 0) {
+//         alert(-1);
+//     } 
+//     else {
+//         alert(0);
+//     }
+// }
+
+
+// checkNumber();
+
+// let result;
+// let a = prompt ("Введите число a");
+// let b = prompt ("Введите число b");
+// result = (a + b < 4) ? 'Мало' : 'Много';
+// alert(result);
+
+
+
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+// alert (message);
+// let login = prompt ("enter your login")
+// let message = (login == 'Сотрудник') ? 'Привет' :
+//   (login == 'Директор') ? 'Здравствуйте' :
+//   (login == '') ? 'Нет логина' :
+//   '';
+//   alert (message);
+
+for (let i = 0; i <= 10; i++){
+    
+    if (!(i % 2)) {
+        console.log(i);
+        
+    }
+}
